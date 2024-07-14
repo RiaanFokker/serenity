09184393aa119e0743f4abadabbf483bbd466e53
Riaan Fokker <77789153+RiaanFokker@users.noreply.github.com>
2024-07-14 09:57:20 +0200

    Photos component

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LightgalleryModule } from 'lightgallery/angular';
 
 @Component({
   selector: 'app-photos',
   standalone: true,
-  imports: [],
+  imports: [LightgalleryModule],
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.css'
 })
