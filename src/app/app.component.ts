d665d35e02c67e0b8326481aa3d8915d18269fb2
Riaan Fokker <77789153+RiaanFokker@users.noreply.github.com>
2024-07-14 09:57:37 +0200

    Amenities done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IntroComponent } from './intro/intro.component';
 import { ContactComponent } from './contact/contact.component';
 import { PhotosComponent } from './photos/photos.component';
+import { RatesComponent } from './rates/rates.component';
+import { AmenitiesComponent } from './amenities/amenities.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, IntroComponent, ContactComponent, PhotosComponent], //ACF RouterOutlet not used atm
+  imports: [RouterOutlet, LandingPageComponent, IntroComponent, ContactComponent, PhotosComponent, RatesComponent, AmenitiesComponent], //ACF RouterOutlet not used atm
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
