3605c61baa8b31fe8be83f2f0bacea37cd26d11b
Riaan Fokker <fokker.riaan@gmail.com>
2024-08-13 15:18:50 +0200

    remove router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { LoadingService } from './services/loading.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideHttpClient(),
     { provide: HTTP_INTERCEPTORS, useClass: LoadingService, multi: true }
   ]
