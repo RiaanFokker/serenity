5bead01d812ca50fa44b662d894a3bfef33ca237
Riaan Fokker <fokker.riaan@gmail.com>
2024-08-26 10:15:43 +0200

    Contact Us footer

--- src/app/outro/outro.component.ts
+++ src/app/outro/outro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-outro',
   standalone: true,
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './outro.component.html',
   styleUrl: './outro.component.css'
 })
