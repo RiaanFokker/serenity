7a0d157f9223ca323ead50fe441a2b1fe8a13432
Riaan Fokker <fokker.riaan@gmail.com>
2024-08-27 08:51:47 +0200

    rename app & remove old favicon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'serenity-ang' title`, () => {
+  it(`should have the 'serenity' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('serenity-ang');
+    expect(app.title).toEqual('serenity');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, serenity-ang');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, serenity');
   });
 });
